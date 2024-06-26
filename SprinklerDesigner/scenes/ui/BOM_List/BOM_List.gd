9dff43e8eb4b2e714b6004608aa6c03de8c8d911
hankedan000 <dany32412@gmail.com>
2024-06-26 08:59:40 -0400

    Uses TablePager from AssetLib for the BOM List

--- SprinklerDesigner/scenes/ui/BOM_List/BOM_List.gd
+++ SprinklerDesigner/scenes/ui/BOM_List/BOM_List.gd
@@ -1,13 +1,75 @@
 extends PanelContainer
 
-@onready var item_list := $ItemList
+signal CellUpdatedSignal(columnName: String, rowIndex: Variant, celValue: Variant)
+signal CellSelectedSignal(columnName: String, rowIndex: Variant)
+
+@onready var tablePager := $TablePager
 
 var _ui_needs_sync = false
 
+# [{manufacturer, model, count}, ...]
+var dataArray: Array[Dictionary] = []
+var lastSortKey
+var lastSortOrder: DataPager.EnumSortOrder
+
+var pageSize = 15
+
+var tableConfig:TableConfig
+
 func _ready():
+	# Tell the table how to get data and what size each page should be.
+	tableConfig = TableConfig.new(
+			SelectPageOfRowData, 
+			GetDataCount, 
+			pageSize)
+	
+	#######################################
+	### Add some columns to the table
+
+	var manuColumnConfig:ColumnConfig = ColumnConfig.new(
+			# Set the Header widget
+			TablePager.CellHeaderResource, 
+			TablePager.CellLabelResource, 
+			"manufacturer", 
+			"row_index")
+	manuColumnConfig.AddCellConfig(ColumnConfig.CELL_SELECTED_SIGNAL, CellSelectedSignal)
+	tableConfig.AddColumnConfig(manuColumnConfig)
+
+	var modelColumnConfig:ColumnConfig = ColumnConfig.new(
+			# Set the Header widget
+			TablePager.CellHeaderResource, 
+			TablePager.CellLabelResource, 
+			"model", 
+			"row_index")
+	modelColumnConfig.AddCellConfig(ColumnConfig.CELL_SELECTED_SIGNAL, CellSelectedSignal)
+	tableConfig.AddColumnConfig(modelColumnConfig)
+	
+	var countColumnConfig:ColumnConfig = ColumnConfig.new(
+			# Set the Header widget
+			TablePager.CellHeaderResource, 
+			TablePager.CellLabelResource, 
+			"count", 
+			"row_index")
+	countColumnConfig.AddCellConfig(ColumnConfig.CELL_SELECTED_SIGNAL, CellSelectedSignal)
+	tableConfig.AddColumnConfig(countColumnConfig)
+	
+	var dataPager: DataPager = DataPager.new(tableConfig)
+	
+	# Initialise the @onready variables 
+	tablePager.Initialise(dataPager)
+	tablePager.Render()
+	
 	TheProject.opened.connect(_on_TheProject_opened)
 	TheProject.closed.connect(_on_TheProject_closed)
 	TheProject.node_changed.connect(_on_TheProject_node_changed)
+
+func _enter_tree():
+	CellUpdatedSignal.connect(_handle_CellUpdatedSignal)
+	CellSelectedSignal.connect(_handle_CellSelectedSignal)
+
+func _exit_tree():
+	CellUpdatedSignal.disconnect(_handle_CellUpdatedSignal)
+	CellSelectedSignal.disconnect(_handle_CellSelectedSignal)
 
 func _process(_delta):
 	if _ui_needs_sync:
@@ -30,33 +92,53 @@ func _sync_ui():
 					'count' : 1
 				}
 	
-	clear_rows()
+	dataArray.clear()
+	var row_index = 0
 	for key in parts_map.keys():
-		var entry = parts_map[key]
-		item_list.add_item(key[0]) # manufacturer
-		item_list.add_item(key[1]) # model
-		item_list.add_item(str(entry['count']))
-
-func row_count(include_header=false) -> int:
-	var rows = int(item_list.item_count / item_list.max_columns)
-	if not include_header:
-		rows -= 1
-	return rows
-
-func clear_rows():
-	while row_count() > 0:
-		remove_row(0)
-
-# remove non-header rows from table (idx 0 is first non-header row)
-func remove_row(row_idx: int):
-	if row_idx >= row_count():
-		push_error("row_idx (%d) >= row_count (%d)" % [row_idx, row_count()])
-		return
+		var value = parts_map[key]
+		dataArray.append({
+			'manufacturer' : key[0],
+			'model' : key[1],
+			'count' : value['count'],
+			'row_index' : row_index})
+		row_index += 1
 	
-	var num_cols = item_list.max_columns
-	var start_idx = num_cols + (row_idx * num_cols)
-	for col_idx in range(num_cols):
-		item_list.remove_item(start_idx)
+	tablePager.Render()
+
+func _handle_CellSelectedSignal(_columnName: String, _rowIndex: Variant):
+	pass
+
+func _handle_CellUpdatedSignal(_columnName: String, _rowIndex: Variant, _cellValue: Variant):
+	pass
+
+func SelectPageOfRowData(_pageSize: int, pageIndex: int, sortKey: String = "", sortOrder: DataPager.EnumSortOrder = DataPager.EnumSortOrder.NONE ) -> Array[Dictionary]:
+	# Fake a database lookup
+	var columnKeyArray: Array = tableConfig.GetKeys()
+	
+	var sortParamsChanged = ( lastSortKey != sortKey || lastSortOrder != sortOrder )
+	if sortKey in columnKeyArray && sortParamsChanged:
+		dataArray.sort_custom( 
+			# Use a Lambda so 'ascending' and 'sortKey' variables can be passed in
+			func(a,b) -> bool: 
+				var aValue = a[sortKey]
+				var bValue = b[sortKey]
+				var lessThan: bool
+				if typeof(aValue) == TYPE_BOOL && typeof(bValue):
+					# boolean requires special compare
+					lessThan = aValue && not bValue if sortOrder else not aValue && bValue
+				else:
+					lessThan = aValue <= bValue if sortOrder else aValue >= bValue
+				return  lessThan
+		)
+		lastSortOrder = sortOrder
+		lastSortKey = sortKey
+		
+	var startIndex = pageIndex * _pageSize
+	var endIndex = startIndex + _pageSize
+	return dataArray.slice(startIndex, endIndex, 1, true)
+
+func GetDataCount() -> int:
+	return dataArray.size()
 
 func _on_TheProject_opened():
 	queue_ui_sync()
