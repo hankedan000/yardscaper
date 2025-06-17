df9ac8fbacab9b41a789f42c5d55cce3e98f50e0
hankedan000 <dany32412@gmail.com>
2025-06-17 08:43:03 -0400

    Adds minor loss lookup table

---
+++ Yardscaper/scripts/PipeTables.gd
@@ -0,0 +1,112 @@
+class_name PipeTables
+extends Object
+
+enum FittingType {
+	ELBOW_90,
+	ELBOW_45,
+	TEE_RUN,
+	TEE_BRANCH
+}
+
+enum MaterialType {
+	PVC
+}
+
+const MINOR_LOSSES = [
+	# PVC minor losses
+	{
+		'material' : MaterialType.PVC,
+		'loss_by_fitting' : [
+			{
+				'fitting' : FittingType.ELBOW_90,
+				'loss_by_diam' : [
+					[0.5,1.6], [0.75,2.1], [1.0,2.6], [1.25,3.5], [1.5,4.0], [2.0,5.2], [2.5,6.2], [3.0,7.7], [4.0,10.1], [5.0,12.6], [6.0,15.2], [8.0,20.0], [10.0,25.1], [12.0,30.0]
+				]
+			},
+			{
+				'fitting' : FittingType.ELBOW_45,
+				'loss_by_diam' : [
+					[0.5,0.8], [0.75,1.1], [1.0,1.4], [1.25,1.8], [1.5,2.2], [2.0,2.8], [2.5,3.3], [3.0,4.1], [4.0,5.1], [5.0,6.7], [6.0,8.1], [8.0,10.6], [10.0,13.4], [12.0,15.9]
+				]
+			},
+			{
+				'fitting' : FittingType.TEE_RUN,
+				'loss_by_diam' : [
+					[0.5,1.0], [0.75,1.4], [1.0,1.7], [1.25,2.3], [1.5,2.7], [2.0,3.5], [2.5,4.1], [3.0,5.1], [4.0,6.7], [5.0,8.4], [6.0,10.1], [8.0,13.3], [10.0,16.7], [12.0,20.0]
+				]
+			},
+			{
+				'fitting' : FittingType.TEE_BRANCH,
+				'loss_by_diam' : [
+					[0.5,3.1], [0.75,4.1], [1.0,5.3], [1.25,6.9], [1.5,8.1], [2.0,10.3], [2.5,12.3], [3.0,16.3], [4.0,20.1], [5.0,25.2], [6.0,30.3], [8.0,40.0], [10.0,50.1], [12.0,58.0]
+				]
+			}
+		]
+	}
+]
+
+enum LossLookupError {
+	# OOR - Out Of Range
+	OK, MaterialOOR, FittingOOR, EmptyLossTable, DiameterOOR
+}
+
+class LossLookupResult:
+	var error : LossLookupError = LossLookupError.OK
+	var loss_factor : float = -1.0
+	
+	static func from_error(err: LossLookupError) -> LossLookupResult:
+		var res := LossLookupResult.new()
+		res.error = err
+		return res
+	
+	static func from_ok_value(value: float) -> LossLookupResult:
+		var res := LossLookupResult.new()
+		res.loss_factor = value
+		return res
+	
+	static func from_warn_value(warn: LossLookupError, value: float) -> LossLookupResult:
+		var res := LossLookupResult.new()
+		res.error = warn
+		res.loss_factor = value
+		return res
+
+static func lookup_minor_loss(material: MaterialType, fitting: FittingType, diam_h: float) -> LossLookupResult:
+	for mat_entry in MINOR_LOSSES:
+		if mat_entry['material'] == material:
+			return _lookup_minor_loss_in_mat_table(mat_entry, fitting, diam_h)
+	return LossLookupResult.from_error(LossLookupError.MaterialOOR)
+
+static func _lookup_minor_loss_in_mat_table(mat_entry: Dictionary, fitting: FittingType, diam_h: float) -> LossLookupResult:
+	for fitting_entry in mat_entry['loss_by_fitting']:
+		if fitting_entry['fitting'] == fitting:
+			return _lookup_minor_loss_in_fitting_table(fitting_entry, diam_h)
+	return LossLookupResult.from_error(LossLookupError.FittingOOR)
+
+static func _lookup_minor_loss_in_fitting_table(fitting_entry: Dictionary, diam_h: float) -> LossLookupResult:
+	var list_of_pairs := fitting_entry['loss_by_diam'] as Array
+	var list_size := list_of_pairs.size()
+	if list_size == 0:
+		return LossLookupResult.from_error(LossLookupError.EmptyLossTable)
+	elif list_size < 2:
+		return LossLookupResult.from_warn_value(LossLookupError.DiameterOOR, list_of_pairs[0][1])
+	
+	# Iterate through pairs
+	for i in range(list_size - 1):
+		var curr = list_of_pairs[i]
+		var next = list_of_pairs[i + 1]
+		
+		if diam_h < curr[0]:
+			# Target is before the first element
+			return LossLookupResult.from_warn_value(LossLookupError.DiameterOOR, curr[1])
+		
+		if curr[0] <= diam_h && diam_h < next[0]:
+			# diam_h is between two entries. interpolate to get loss factor
+			return LossLookupResult.from_ok_value(_lookup_minor_loss_lerp(curr, next, diam_h))
+	
+	# diam_h is beyond the last element return last loss factor
+	return LossLookupResult.from_warn_value(LossLookupError.DiameterOOR, list_of_pairs[list_size - 1][1])
+	
+static func _lookup_minor_loss_lerp(entry_a: Array, entry_b: Array, diam_h: float) -> float:
+	var diam_spread = entry_b[0] - entry_a[0] as float
+	var weight := (diam_h - entry_a[0]) / diam_spread  as float
+	return lerpf(entry_a[1], entry_b[1], weight)
